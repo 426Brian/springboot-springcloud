252b83a771910e02672aa6fedfdcb60156817f4f
426Brian <1165380405@qq.com>
2019-12-29 18:57:51 +0800

    springboot-springcloud

---
+++ springboot-springcloud/provider-ticket/src/main/java/com/briancloud/providerticket/service/TicketService.java
@@ -0,0 +1,11 @@
+package com.briancloud.providerticket.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TicketService {
+
+    public String getTicket(){
+        return "<东方快车谋杀案>";
+    }
+}
